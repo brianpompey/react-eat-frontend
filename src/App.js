2604fda2a2e2d8ca264a5bc0f61d3049ee5d4636
brianpompey <brianthescholar@gmail.com>
2020-11-14 15:19:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header.js';
+import './App.css';
+
+function App() {
+  return (
+    <div id="root">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <div>
+                <h1>What To Eat Today!</h1>
+                <hr />
+            </div>
+        )
+    }
+};
+
+export default Header;
