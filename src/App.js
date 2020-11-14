47ccd9f0c14697dbeb10555da04d498b54c862e3
brianpompey <brianthescholar@gmail.com>
2020-11-14 17:19:47 -0500

    implementing bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header.js';
+import Navbar from './components/Navbar.js';
+import Buttoner from './components/Button.js';
 import './App.css';
 
 function App() {
   return (
     <div id="app">
       <Header />
+      <Buttoner />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+class Buttoner extends Component {
+    render() {
+        return(
+            <>
+                <Button variant="primary">Primary</Button>{' '}
+            </>    
+        )   
+    }
+}
+
+export default Buttoner;
