e24d91f531ea009b1d666377e23bdf3dea5b66be
brianpompey <brianthescholar@gmail.com>
2020-11-26 11:01:43 -0500

    links work just need actions to work

--- src/actions/fetchRestaurants.js
+++ src/actions/fetchRestaurants.js
@@ -2,7 +2,7 @@ export function fetchRestaurants() {
     return (dispatch) => {
       fetch('http://localhost:3000/api/v1/restaurants')
       .then(resp => resp.json())
-      .then(accounts => dispatch({
+      .then(restaurants => dispatch({
         type: 'FETCH_RESTAURANTS',
         payload: restaurants
       }))
