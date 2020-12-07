40d3a8ae121737e45f5888af7090a6dede94c15a
brianpompey <brianthescholar@gmail.com>
2020-12-06 22:43:47 -0500

    working on selections container

--- src/containers/SelectionsContainer.js
+++ src/containers/SelectionsContainer.js
@@ -1,2 +1,20 @@
-mport React, { Component } from 'react';
+import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import OrderSelection from '../components/OrderSelection.js'
+
+class SelectionsContainer extends Component {
+
+
+    render() {
+      return (
+        <div>
+            <OrderSelection restaurant={this.props.restaurant}/><br/>
+        </div>
+      )
+  
+    }
+  
+  }
+  
+  export default SelectionsContainer;
+
