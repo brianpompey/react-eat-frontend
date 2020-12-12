ff08da6c8777fd740dbbec7cc3edfd85ccdfad1c
brianpompey <brianthescholar@gmail.com>
2020-12-11 19:11:10 -0500

    sort and unsort button

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Restaurants = (props) => {
-    console.log(props.restaurants);
 
-  return (
-    <div>
-      {props.restaurants.map(rest =>
-        <>
-        <li key={rest.id} style={{textAlign: 'center'}}>
-            
-            <a href={rest.website} target="_blank" rel="noreferrer">{rest.name}</a> - {rest.cuisine} Cuisine in {rest.location}
-        </li>
-        <br />
-        </>
-      )}
-    </div>
-    )
+class Restaurants extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      restaurants: []
+    }
+  }
+
+  onClick1 = () => {
+    this.setState({
+      restaurants: this.props.restaurants.sort((a, b) => a.name.localeCompare(b.name))
+    })
+  }
+
+  onClick2 = () => {
+    this.setState({
+      restaurants: this.props.restaurants.sort((a, b) => a.id - b.id)
+    })
+  }
+
+  render() {
+  //  console.log(this.props.restaurants.sort((a, b) => a.name.localeCompare(b.name)))
+    return (
+      <div>
+        <button onClick={this.onClick1}>SORT</button>
+        <button onClick={this.onClick2}>UNSORT</button>
+        <br/>
+        {this.props.restaurants.map(rest =>
+          <>
+          <li key={rest.id} style={{textAlign: 'center'}}>
+              
+              <a href={rest.website} target="_blank" rel="noreferrer">{rest.name}</a> - {rest.cuisine} Cuisine in {rest.location}
+          </li>
+          <br />
+          </>
+        )}
+      </div>
+      )
+        }
 }
 
 export default Restaurants;
